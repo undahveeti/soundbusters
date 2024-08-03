b795811230ee0e63ccebf96712957126fd2073aa
Maxim Alayev <neurologicalstyle@gmail.com>
2024-08-03 16:30:43 -0700

    follower not friend

--- database/friend.sql
+++ database/friend.sql
@@ -1,9 +1,8 @@
 USE `mysql`;
 
-CREATE TABLE IF NOT EXISTS `friend` (
-  `friendA` int(64) NOT NULL,
-  `friendB` int(64) NOT NULL,
-  CONSTRAINT fk_friendA FOREIGN KEY (friendA) REFERENCES user(id),
-  CONSTRAINT fk_friendB FOREIGN KEY (friendB) REFERENCES user(id),
-  CONSTRAINT uq_friends UNIQUE (LEAST(friendA, friendB), GREATEST(friendA, friendB))
+CREATE TABLE IF NOT EXISTS `follower` (
+  `user` BIGINT NOT NULL,
+  `follower` BIGINT NOT NULL,
+  CONSTRAINT fk_user FOREIGN KEY (user) REFERENCES user(id),
+  CONSTRAINT fk_follower FOREIGN KEY (follower) REFERENCES user(id)
 )
