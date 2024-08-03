b6dbb7fd77d7a8eff6833eafc09fc3bc56b9600e
irfan61802 <irfan61802@gmail.com>
2024-08-03 16:33:10 -0700

    uq follower

--- database/follower.sql
+++ database/follower.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS `follower` (
   `user` BIGINT NOT NULL,
   `follower` BIGINT NOT NULL,
   CONSTRAINT fk_user FOREIGN KEY (user) REFERENCES user(id),
-  CONSTRAINT fk_follower FOREIGN KEY (follower) REFERENCES user(id)
+  CONSTRAINT fk_follower FOREIGN KEY (follower) REFERENCES user(id),
+  CONSTRAINT uq_follower UNIQUE (user, follower)
 )
