774392e3c108a13d3e811602c87bf4247a991f9a
undahveeti <undahvt@gmail.com>
2024-08-03 15:03:26 -0700

    initial commit / login screen

---
+++ front/LoginPageUI/LoginPageUI/LoginPageUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageUIApp.swift
+//  LoginPageUI
+//
+//  Created by Andy Tran on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
