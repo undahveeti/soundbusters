4e8d993f6a949deaac9fc1a192271bc03451a4c5
undahveeti <undahvt@gmail.com>
2024-08-03 16:04:18 -0700

    moved frontend to different folder

---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by Andy Tran on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
