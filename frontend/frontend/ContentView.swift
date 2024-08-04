ed879f28f4b2d9b868d56adefc4a9113aa25d64a
undahveeti <undahvt@gmail.com>
2024-08-03 21:08:26 -0700

    updated ContentView with welcome screen

--- frontend/frontend/ContentView.swift
+++ frontend/frontend/ContentView.swift
@@ -9,83 +9,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     @State private var username = ""
     @State private var password = ""
     @State private var wrongUsername = 0
     @State private var wrongPassword = 0
-     
-    @State private var showingLoginScreen = false
     
-    var body: some View {
-        NavigationView{
-            ZStack{
-                Color.purple
-                    .ignoresSafeArea()
-                Circle()
-                    .scale(1.7)
-                    .foregroundColor(.white.opacity(0.15))
-                Circle()
-                    .scale(1.5)
-                    .foregroundColor(.white)
-                VStack{
-                    Text("Login")
-                        .font(.largeTitle)
-                        .bold()
-                        .padding()
-                    TextField("Username", text: $username)
-                        .padding()
-                        .frame(width:300, height:50)
-                        .background(.black.opacity(0.05))
-                        .cornerRadius(10)
-                        .border(.red, width: CGFloat(wrongUsername))
-                    SecureField("Password", text: $password)
-                        .padding()
-                        .frame(width:300, height: 50)
-                        .background(.black.opacity(0.05))
-                        .cornerRadius(10)
-                        .border(.red, width: CGFloat(wrongPassword))
-                    
-                    Button("Login")
-                    {
-                        authenticateUser(username: username, password: password)
-                    }
-                    .foregroundColor(.white)
-                    .frame(width:300, height: 50)
-                    .background(Color.purple)
-                    .cornerRadius(10)
-                    
-                    NavigationLink(destination: LoginScreen(username: username), isActive: $showingLoginScreen){
-                        EmptyView()
-                    }
-                    
-                        
-                }
-            }
-        }
-        .navigationBarHidden(true)
+    @State private var showingLoginScreen = false
+    var body : some View {
+        welcomeScreen()
     }
-    func authenticateUser(username: String, password: String)
-    {
-        if username.lowercased() == "georgethedog"
-        {
-            wrongUsername = 0
-            if password.lowercased() == "woof"
-            {
-                wrongPassword = 0
-                showingLoginScreen = true
-            }
-            else
-            {
-                wrongPassword = 2
-            }
-        }
-        else
-        {
-            wrongUsername = 2
-        }
+    
+}
+
+
+struct PrimaryButton: View {
+    var title: String
+    var body : some View{
+        NavigationLink(
+            destination: loginScreen(),
+            label: {
+                Text(title)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(.indigo.opacity(0.50))
+                    .cornerRadius(50)
+                
+                
+            })
     }
 }
 
+struct SocialLoginButton: View {
+    var image: Image
+    var text: Text
+    var body : some View {
+        VStack{
+            HStack{
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 65, height: 65)
+                    .padding(.horizontal)
+                text
+                    .font(.title2)
+            
+                
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.green.opacity(0.20))
+            .cornerRadius(50)
+            
+            
+        }
+    }
+}
 #Preview {
     ContentView()
 }
+
+
+
