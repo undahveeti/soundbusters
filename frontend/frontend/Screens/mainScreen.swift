b3d7905862cf9dd49d4f300349e7fcb2bdb76740
undahveeti <undahvt@gmail.com>
2024-08-03 21:07:18 -0700

    mainScreen created

--- frontend/frontend/Screens/mainScreen.swift
+++ frontend/frontend/Screens/mainScreen.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct mainScreen : View {
+    let username : String
+    var body : some View {
+        VStack{
+            
+            Text("Welcome, \(username) to Frequency!")
+                .font(.largeTitle)
+                .bold()
+            
+        }
+        .navigationTitle("Login Success")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+}
+
+#Preview {
+    mainScreen(username: "SampleUser")
+}
