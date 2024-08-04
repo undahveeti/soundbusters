0b35067bbcd8c7d18e5e7fe10df6499e61784129
undahveeti <undahvt@gmail.com>
2024-08-03 21:06:06 -0700

    finished SignInScreen' git add frontend/frontend/Screens/SignInScreen.swift git add frontend/frontend/Screens/SignInScreen

--- frontend/frontend/Screens/SignInScreen.swift
+++ frontend/frontend/Screens/SignInScreen.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct SignInScreenView: View {
+    var body : some View {
+        VStack{
+            Text("Sign Up")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            SocialLoginButton(image: Image(.spotifyLogo), text: Text("Sign up with Spotify"))
+        }
+    }
+}
+
+
+#Preview {
+    SignInScreenView()
+}
